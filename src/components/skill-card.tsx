a261d326a741ba6b5a3add77ad91b8eaa5e7cc76
Yurith Rubio <iamyurith@gmail.com>
2024-03-14 23:12:09 +0100

    comment icon

--- src/components/skill-card.tsx
+++ src/components/skill-card.tsx
@@ -10,7 +10,7 @@ export function SkillCard({ icon: icon, title, children }: SkillCardProps) {
   return (
     <Card color="transparent" shadow={false} placeholder={undefined} >
       <CardBody className="grid justify-center text-center" placeholder={undefined} >
-        <img src={icon} className="mx-auto h-12 w-12" alt={icon} />
+        {/* <img src={icon} className="mx-auto h-12 w-12" alt={icon} /> */}
         {/* <div className="mx-auto mb-6 grid h-12 w-12 place-items-center rounded-full bg-deep-purple-500 p-2.5 text-white shadow">
           <Icon className="h-6 w-6" strokeWidth={2} />
         </div> */}
