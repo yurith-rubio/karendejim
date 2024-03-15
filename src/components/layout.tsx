9e9a63410e06dcafb448a50a02fdedda3816e84d
Yurith Rubio <iamyurith@gmail.com>
2024-03-15 19:50:17 +0100

    ignore React.ReactNode type

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  {/* @ts-ignore */}
   return <ThemeProvider>{children}</ThemeProvider>;
 }
 
