bcf860416955d34b0cf5f04147b541862904a8c2
Yurith Rubio <iamyurith@gmail.com>
2024-03-27 18:49:01 +0100

    video update

--- src/app/motion.tsx
+++ src/app/motion.tsx
@@ -17,7 +17,7 @@ export function Motion() {
             </div>
             <div className="container-video">
                 <iframe className="responsive-iframe" width="800" height="auto"
-                src="https://cdn.myportfolio.com/b57c2722-2985-4260-9e1c-785fd5d931c3/1eded75e-b6fd-43cc-80bf-da2b89e3a3f4_rw_1200.gif?h=70427dfa038ef5b0e4c1130a352b4ae4">
+                src="https://www-ccv.adobe.io/v1/player/ccv/4JNRCBVou5I/embed?bgcolor=%23191919&lazyLoading=true&api_key=BehancePro2View">
                 </iframe> 
             </div>
             <div className="container-video">
