92716b50ad5c0e3e5c3aa6192d72910aa6936a87
Yurith Rubio <iamyurith@gmail.com>
2024-07-29 22:53:04 +0200

    add thesis project

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -11,14 +11,14 @@ export default function Page({ params }: { params: { projectId: any } }){
     if (selectedProject) {
       return (
           <>
-            <div className="flex justify-center">
+            <div className="projects-container flex justify-center">
               <div className="bg-gray-50 flex lg:max-w-screen-xl m-auto w-full fixed top-0 z-10 self-center">
                 <Link href="/#projects" className="ml-auto close"  >
                   <Image src="/icons/close.svg" className="w-8 h-8" alt="close icon" width={30} height={30} />
                 </Link>
               </div>
 
-              <div className="bg-gray-50 grid gap-9 p-8">
+              <div className="content bg-gray-50 grid gap-9 p-8">
                 {/* @ts-ignore */}
                 <p className="text-xl mb-4 max-w-3xl mr-auto ml-auto pt-16" >
                       { selectedProject.description && selectedProject.description }
@@ -31,7 +31,7 @@ export default function Page({ params }: { params: { projectId: any } }){
                         alt={url}
                         width={768}
                         height={768}
-                        className="object-cover rounded-xl mr-auto ml-auto"
+                        className="image-project object-cover rounded-xl mr-auto ml-auto"
                         />
                       </a>
                     </div>
