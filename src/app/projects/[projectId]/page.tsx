50eb88c482c628320911536e7aa34b08b62124bb
Yurith Rubio <iamyurith@gmail.com>
2024-08-01 17:55:25 +0200

    video code added

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -47,7 +47,22 @@ export default function Page({ params }: { params: { projectId: any } }) {
                     </a>
                   </div>
                 );
-              } 
+              } else {
+                return (
+                  <div key={key} className="container-video-project">
+                    <iframe
+                      className="iframe-video"
+                      width="560"
+                      height="315"
+                      src={url}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                );
+              }
             })}
             {selectedProject.title === "Set of Illustrations for ITONICS" && (
               <div className="container-video">
